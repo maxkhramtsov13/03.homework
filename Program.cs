ff1ee66b9d1564fd7bb0ae068123471e5d79225e
MaxKhramtsov <maxkhramtsov13@gmail.com>
2023-02-13 16:17:53 +0300

    homework03

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿//Напишите программу, которая принимает на вход три числа
+// и выдаёт максимальное из этих чисел.
+
+internal class Program
+{
+	private static void Main(string[] args)
+	{
+		int a = new Random().Next(-99 , 99);
+		Console.Write($" Первое число -> ");
+		Console.WriteLine(a);
+		
+		int b = new Random().Next(-99 , 99);
+		Console.Write($" Второе число -> ");
+		Console.WriteLine(b);
+		
+		int c = new Random().Next(-99 , 99);
+		Console.Write($" Третье число -> ");
+		Console.WriteLine(c);
+
+		int max = a;
+
+		if( a > max ) max = a;
+		if( b > max ) max = b;
+		if( c > max ) max = c;
+
+		System.Console.Write($" Максимальное число -> ");
+		Console.WriteLine(max);
+	}	
+}
